7270c4f6bc8cb322f58d34e4c3d8646ef02320c1
Ulan Seitkaliyev <ulanseitkaliyev@cmu.edu>
2025-03-08 23:11:43 +0000

    fix schema

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -66,8 +66,8 @@ CREATE TABLE message (
     message_type message_type NOT NULL DEFAULT 'text',
     timestamp TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     sender_id VARCHAR(255) NOT NULL,
-    is_from_bot BOOLEAN NOT NULL DEFAULT FALSE
-    is_complete BOOLEAN NOT NULL DEFAULT FALSE;
+    is_from_bot BOOLEAN NOT NULL DEFAULT FALSE,
+    is_complete BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 -- Create index on conversation_id and timestamp for faster message history retrieval
